54c0c8ebdc2635d3317ab145de199052cd150e7e
abdul <abdullahplayz07@gmail.com>
2021-08-25 22:08:43 +0100

    changed the read me thingy majigy

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is something I had to do because its the holidays and I have nothing better to do :O
+
+See the deployed version (via firebase ✌) - https://prac-dc61d.web.app/
---
+++ build/precache-manifest.d5179bcafd623e613bb0b1c99351e4cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e03e2ac7401a5cd790d47e35d967062f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bacabe84d789fc088332",
+    "url": "/static/css/main.96d890a0.chunk.css"
+  },
+  {
+    "revision": "7c23a4b7905711b9944e",
+    "url": "/static/js/2.8b18bc4f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.8b18bc4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bacabe84d789fc088332",
+    "url": "/static/js/main.f07b0293.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
